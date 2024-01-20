788589a95a9993ba43675fb00e30413c74006d6d
rafimaryudwika <northernosprey2013@gmail.com>
2024-01-20 17:07:24 +0700

    Remove image optimizer from strapi-server

--- src/extensions/upload/strapi-server.ts
+++ src/extensions/upload/strapi-server.ts
@@ -1,6 +1,6 @@
-import imageOptimizerService from "strapi-plugin-image-optimizer/dist/server/services/image-optimizer-service";
+// import imageOptimizerService from "strapi-plugin-image-optimizer/dist/server/services/image-optimizer-service";
 
-module.exports = (plugin) => {
-  plugin.services["image-manipulation"] = imageOptimizerService;
-  return plugin;
-};
+// module.exports = (plugin) => {
+//   plugin.services["image-manipulation"] = imageOptimizerService;
+//   return plugin;
+// };
