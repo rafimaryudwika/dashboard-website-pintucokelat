f6be85ebf6fae8e5403a6adbf4d013ddd3f04c8e
rafimaryudwika <northernosprey2013@gmail.com>
2024-03-31 07:05:14 +0700

    [feat] remove email sending

--- src/api/ph-quote-list/content-types/ph-quote-list/lifecycle.ts
+++ src/api/ph-quote-list/content-types/ph-quote-list/lifecycle.ts
@@ -1,17 +1,15 @@
-import nodemailer from 'nodemailer';
-
 export default ({env}) => ({
-    async afterCreate(event) {
-        const {result} = event;
-        try {
-            await strapi.plugin['email'].services.email.send({
-                to: result.Email,
-                from: env('SMTP_USERNAME'),
-                subject: ``, //TODO Minta saran ke owner
-                html: ``, //TODO Minta saran ke owner
-            })
-        } catch (err) {
-            console.error(err);
-        }
-    }
+    // async afterCreate(event) {
+    //     const {result} = event;
+    //     try {
+    //         await strapi.plugin['email'].services.email.send({
+    //             to: result.Email,
+    //             from: env('SMTP_USERNAME'),
+    //             subject: ``, //TODO Minta saran ke owner
+    //             html: ``, //TODO Minta saran ke owner
+    //         })
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // }
   });
